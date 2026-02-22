4fc67eb9558acc24a46a20b0663c3f2a32f1d8d6
Ashish Mathew <ashishmathew@example.com>
2026-02-22 20:34:48 +0530

    Added Question2 python script

---
+++ Question2.py
@@ -0,0 +1,11 @@
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+print("Sum of",n1,"and",n2,"is =", n1+n2)
+print("Difference of",n1,"and",n2,"is =", n1-n2)
+print("Product of",n1,"and",n2,"is =", n1*n2)
+if n2!=0:
+    print("Division of",n1,"and",n2,"is =", n1/n2)
+    print("Modulus of",n1,"and",n2,"is =", n1%n2)
+else:
+    print("divisor cannot be 0")
+print("Exponent of",n1,"and",n2,"is =", n1**n2)
