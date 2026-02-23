932f7a032109b9bd60ce10de4882e5d89667dfce
Ashish Mathew <ashish.mathew2021@gmail.com>
2026-02-23 08:30:56 +0530

    Add files via upload

---
+++ Question1.py
@@ -0,0 +1,17 @@
+#Bio Card
+#declaring Variables with different details
+name="Ashish Mathew"
+age=22
+course="Gen AI"
+college="CMRIT"
+email="asma22cs@cmrit.ac.in"
+#Student card
+print("|-------------------------------------|")
+print("|          Student BIO Card           |")
+print("|-------------------------------------|")
+print(f"|Name: {name}                  |")
+print(f"|Age: {age}                              |")
+print(f"|Course: {course}                       |")
+print(f"|College: {college}                       |")
+print(f"|Email: {email}          |")
+print("|-------------------------------------|")
